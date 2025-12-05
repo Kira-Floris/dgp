72827cf35831553d1bef9dcf385c6591a8d1cee7
Kira-Floris <nzafloris@gmail.com>
2025-12-05 09:24:14 +0000

    refactor: reformating data saving for wikipedia

--- dgp/datasets/wikipedia.py
+++ dgp/datasets/wikipedia.py
@@ -1,31 +1,35 @@
 from datasets import load_dataset
 import os
 import json
+from tqdm import tqdm
+import re
 
 save_dir = "./data/"
 os.makedirs(save_dir, exist_ok=True)
 
-def load_wikipedia_dataset(split: str = "train", data_dir: str = "20231101.en"):
-    """
-    Load the Wikipedia dataset from Hugging Face Datasets.
-    
-    Args:
-        split: Dataset split to load (e.g., "train", "validation", "test")
-        
-    Returns:
-        Loaded dataset split
-    """
+def load_wikipedia_dataset(
+        split: str = "train",
+        data_dir: str = "20231101.simple",
+    ):
     dataset = load_dataset("wikimedia/wikipedia", data_dir, split=split)
-    # save the dataset to ./data/ only saving the text column as json file
-    jsonl_path = os.path.join(save_dir, f"wikipedia_{split}.jsonl")
-    with open(jsonl_path, "w", encoding="utf-8") as f:
-        for row in dataset:
-            text = row.get("text", "")
-            f.write(json.dumps({"text": text}, ensure_ascii=False) + "\n")
 
-    print(f"Saved text column to {jsonl_path}")
-
-    return dataset
+    save_folder = os.path.join(save_dir, "wikimedia--wikipedia", data_dir, "seed")
+    os.makedirs(save_folder, exist_ok=True)
+    text_lens = []
+    sentence_lens = []
+    for i, row in tqdm(enumerate(dataset), total=len(dataset), desc=f"Saving {data_dir} to {save_folder}"):
+        text: str = row.get("text", "")
+        text_lens.append(len(text.split()))
+        sentence_lens.append(len(re.findall(r".+?[.!?]+", text)))
+        file_name = f"row_{i}.txt"
+        # with open(os.path.join(save_folder, file_name), "w", encoding="utf-8") as f:
+        #     f.write(text)
+    print(f"Average Text Length: {sum(text_lens)/len(text_lens)}")
+    print(f"Min Text Length: {min(text_lens)}")
+    print(f"Max Text Length: {max(text_lens)}")
+    print(f"Average Sentences: {sum(sentence_lens)/len(sentence_lens)}")
+    print(f"Min Sentences: {min(sentence_lens)}")
+    print(f"Max Sentences: {max(sentence_lens)}")
 
 if __name__ == "__main__":
     load_wikipedia_dataset(split="train")
